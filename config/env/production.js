46e2ff8c7c963baa678aa5ed05c7bd6637b44ee8
xgharibyan <khachatur.gharibyan@gmail.com>
2017-01-25 14:04:14 +0400

    RO-4 # Update production env db configs

--- config/env/production.js
+++ config/env/production.js
@@ -1,11 +1,17 @@
 export default {
-  env: 'production',
-  jwtSecret: '4DNSrMPuQ3Y3McBu96wd2GzGheDXuft8gDqLEQVWHnXQfcaGFtM2ZBgyNYzPN7CK',
-  db: 'mongodb://localhost/rodin-js-api-production',
-  modules:{
-     socketService:{
-         URL:'https://modules.rodin.io',
-         port:4000,
-     }
-  },
+    env: 'production',
+    jwtSecret: '4DNSrMPuQ3Y3McBu96wd2GzGheDXuft8gDqLEQVWHnXQfcaGFtM2ZBgyNYzPN7CK',
+    db: {
+        username: 'root',
+        host: '178.62.229.191',
+        port: 22,
+        dstPort: 27017,
+        url: 'mongodb://localhost:27017/rodin-js-api-development'
+    },
+    modules: {
+        socketService: {
+            URL: 'https://modules.rodin.io',
+            port: 4000,
+        }
+    },
 };
